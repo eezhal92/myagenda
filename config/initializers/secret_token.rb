e7112d9259119f6cf0d754301764de76dd2e6af9
eezhal92 <arkinthesky.69@gmail.com>
2014-07-31 19:42:05 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Myagenda::Application.config.secret_key_base = '70dc4ad96c45933e3d1d9438565f38edceaeffd09f4705c01ccc9270eed130f6c3bf099e83b0f8dad0260b77226385cb4007221bb5e8faa97f758055c910210f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Myagenda::Application.config.session_store :cookie_store, key: '_myagenda_session'
