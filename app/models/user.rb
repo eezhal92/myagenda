b2480af8401cf550f43c24de337903b604c3ed02
eezhal92 <arkinthesky.69@gmail.com>
2014-08-02 07:11:06 -0400

    set role _id to 2 for new user

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,12 +5,16 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable #, :confirmable
 
   has_many :agendas 
-
   belongs_to :role
-
+  before_create :set_default_role
 
   # @parama = string (administrator or general_user)
   def has_role?(role)
    	self.role.name == role   	
   end
+
+  private
+    def set_default_role
+      self.role ||= Role.find_by_name('general_user')
+    end
 end
